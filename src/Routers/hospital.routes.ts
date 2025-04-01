778743e83dbabdc33115a5b41b453833db13e90d
codenuk <supagorn00001@gmail.com>
2025-04-01 09:18:43 +0700

    Update

--- src/Routers/hospital.routes.ts
+++ src/Routers/hospital.routes.ts
@@ -1,5 +1,5 @@
-const express = require("express");
-const router = express.Router();
+import { Request, Response } from 'express';
+import router from '../configs/expressClient';
 
 // mock data
 const rooms = [
@@ -15,30 +15,30 @@ const rooms = [
   },
 ];
 
-router.get("/", (req, res) => {
+router.get("/", (req: Request, res: Response) => {
   res.json(rooms);
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", (req: Request, res: Response) => {
   const { id } = req.params;
   const result = rooms.find((product) => product.id === id);
   res.json(result);
 });
 
-router.post("/", (req, res) => {
+router.post("/", (req: Request, res: Response) => {
   const payload = req.body;
   res.json(payload);
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", (req: Request, res: Response) => {
   const { id } = req.params;
   const payload = req.body;
   res.json(payload);
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", (req: Request, res: Response) => {
   const { id } = req.params;
   res.json({ id });
 });
 
-module.exports = router;
+export default router
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser');
+import express, { Express, Request, Response, NextFunction } from 'express';
+import bodyParser from 'body-parser';
+
+const app: Express = express()
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -8,19 +9,19 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-const hospitalRouter = require('./Routers/hospital.routes');
+import hospitalRouter from './Routers/hospital.routes';
 
 app.use('/hospital', hospitalRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   res.status(err.status || 404).json({
     message: "No such route exists"
   })
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   res.status(err.status || 500).json({
     message: "Error Message"
   })
